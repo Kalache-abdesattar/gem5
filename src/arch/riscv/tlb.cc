762f431838252e56eb04934dd9e7fa4be3132b42
Kalache-abdesattar <abdesattar.kalache@tuni.fi>
2025-07-13 21:20:19 +0000

    bypass MMU permission checks for RISCV

--- src/arch/riscv/tlb.cc
+++ src/arch/riscv/tlb.cc
@@ -302,6 +302,9 @@ TLB::checkPermissions(ThreadContext* tc, MemAccessInfo mem_access, Addr vaddr,
         }
     }
 
+    // bypass permission checks
+    pf = false;
+
     return pf ? createPagefault(vaddr, mode, gvaddr, gpf, virt) : NoFault;
 }
 
